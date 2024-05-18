6a9d66387cb726c19f717e4391d9063bc028c80e
saeed <saeed@eighty6.shop>
2024-05-18 17:06:06 +0500

    fix: more changes

--- src/config/systemConfigs.js
+++ src/config/systemConfigs.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://13.51.150.207:3002/api";
-export const SERVE_HEALTH = "http://13.51.150.207:3002/api/health";
+export const BASE_URL = "http://13.60.56.191:3001/api";
+export const SERVE_HEALTH = "http://13.60.56.191:3001/api/health";
