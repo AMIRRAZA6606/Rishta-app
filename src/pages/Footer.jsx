fc3fbe6fa28ab9817f5cf9c84857eeb375eb6ada
Amir Raza <amirrazadepalpur@gmail.com>
2024-05-18 18:27:30 -0700

    footer update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,38 +5,45 @@ import email from '../assets/icons/email.png'
 const Footer = () => {
     return (
         <div className='footer-main-con'>
-<div className="footerlogo">
-<img src={footerLogo} alt="" />
-</div>
+            <div className="footerlogo">
+                <img src={footerLogo} alt="" />
+            </div>
 
-<hr className='row'/>
+            <hr className='row' />
+            <div className='footer-details'>
+                <div className='reach-us-con'>
+                    <span className='f-title'>Reach Us</span>
+                    <div className='desc'>
+                        <img src={call} alt="" className='call' />
 
-<div className="contact-main">
-    <div className="reachus">
-        <h3>Reach</h3>
-        <h3 className='company'>Company</h3>
-    </div>
+                        <span>+92 0000000000</span>
+                    </div>
+                    <div className='desc'>
+                        <img src={email} alt="" />
 
-    <div className="reachus">
-        <div className="mobile">
-            <img src={call} alt="call" />
-            <p>+920000000000</p>  
+                        <span>demo@gmail.com</span>
+                    </div>
+
+                </div>
+                <div className='company-con'>
+                    <span>Company</span>
+
+
+                    <div className='f-details-2'>
+                        <span>About</span>
+
+                        <span>Contact Us</span>
+                    </div>
+                </div>
+            </div>
+            <div className='rights-con'>
+                <p className='footerpar'>All rights reserved 2024 © <span>Privacy Policy</span>
+                </p>
+            </div>
+
         </div>
-        <h3>About</h3>
-    </div>
-    <div className="reachus">
-        <div className="Email">
-            <img className='email' src={email} alt="call" />
-            <p>demo@gmail.com</p>  
-        </div>
-        <h3>Contact-us</h3>
-    </div>
-   
-</div>
-<p className='footerpar'>All rights reserved 2024 @ <span>privacy policy</span></p>
-</div>
 
-  
+
     )
 }
 
