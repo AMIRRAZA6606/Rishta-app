94d00af03464853a303a4741e2233728e0034176
saeed <saeed@eighty6.shop>
2024-05-31 17:41:55 +0500

    fix: change

--- src/config/systemConfigs.js
+++ src/config/systemConfigs.js
@@ -1,7 +1,7 @@
 // =================PROD=====================
-// export const BASE_URL = "http://13.60.56.191:3001/api";
-// export const IMAGE_BASE_URL = "http://13.60.56.191:3001/uploads";
+export const BASE_URL = "http://13.60.56.191:3001/api";
+export const IMAGE_BASE_URL = "http://13.60.56.191:3001/uploads";
 
 // =================LOCAL====================
-export const BASE_URL = "http://localhost:3001/api";
-export const IMAGE_BASE_URL = "http://localhost:3001/uploads";
+// export const BASE_URL = "http://localhost:3001/api";
+// export const IMAGE_BASE_URL = "http://localhost:3001/uploads";
