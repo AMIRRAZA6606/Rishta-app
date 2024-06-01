f3272e857dab63407a20b4c4042873f291033ae4
saeed <saeed@eighty6.shop>
2024-06-01 20:24:52 +0500

    fix: more changes

--- src/pages/friends-page/Friends.jsx
+++ src/pages/friends-page/Friends.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import { Oval } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import chatIcon from "../../assets/icons/chat.png";
 import viewProfileIcon from "../../assets/icons/view-profile.png";
-import fallbackPersonImg from "../../assets/images/fallbackPersonImg.png";
 import { getMyFriends } from "../../services/friends";
 import "./friends.css";
 import { IMAGE_BASE_URL } from "../../config/systemConfigs";
@@ -63,94 +63,116 @@ const FriendsListing = () => {
 
   return (
     <>
-      <div className="connection-main-con">
-        <div className="my-friend-class">
-          <div className="connection-list-con">
-            {friends && friends.length > 0
-              ? friends.map((friend, index) => (
-                  <div key={index} className="connection-con">
-                    <div className="connection-img">
-                      <img src={`${IMAGE_BASE_URL}/${friend.image}`} alt="" />
-                    </div>
-                    <div className="connection-info">
-                      <p>
-                        <strong>Name:</strong>{" "}
-                        {`${friend.firstName} ${friend.lastName}`}
-                      </p>
-                      <p>
-                        <strong>Nickname:</strong> {friend.nickName}
-                      </p>
-                      <div className="lastseen-con"></div>
-                      <div className="border-1"></div>
-                      <div className="desc-con-wrapper">
-                        <div className="info-con">
-                          <p>
-                            <strong>Tongue:</strong>
-                            {friend.tongue}
-                          </p>
-                          <p>
-                            <strong>Address:</strong>
-                            {friend.address}
-                          </p>
+      <div>
+        <div className="connection-main-con">
+          {loading ? (
+            <div className="loader">
+              <Oval
+                height={80}
+                width={80}
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#4fa94d"
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+              />
+            </div>
+          ) : (
+            <div className="my-friend-class">
+              <div className="connection-list-con">
+                {friends && friends.length > 0
+                  ? friends.map((friend, index) => (
+                      <div key={index} className="connection-con">
+                        <div className="connection-img">
+                          <img
+                            src={`${IMAGE_BASE_URL}/${friend.image}`}
+                            alt=""
+                          />
                         </div>
-                        <div className="info-con">
+                        <div className="connection-info">
                           <p>
-                            <strong>Religion:</strong>
-                            {friend.religion}, <strong>Cast:</strong>
-                            {friend.cast}
+                            <strong>Name:</strong>{" "}
+                            {`${friend.firstName} ${friend.lastName}`}
                           </p>
+                          <p>
+                            <strong>Nickname:</strong> {friend.nickName}
+                          </p>
+                          <div className="lastseen-con"></div>
+                          <div className="border-1"></div>
+                          <div className="desc-con-wrapper">
+                            <div className="info-con">
+                              <p>
+                                <strong>Tongue:</strong>
+                                {friend.tongue}
+                              </p>
+                              <p>
+                                <strong>Address:</strong>
+                                {friend.address}
+                              </p>
+                            </div>
+                            <div className="info-con">
+                              <p>
+                                <strong>Religion:</strong>
+                                {friend.religion}, <strong>Cast:</strong>
+                                {friend.cast}
+                              </p>
+                            </div>
+                            <p>
+                              <strong>Education:</strong>
+                              {friend.education ? friend.education : ""}
+                            </p>
+                          </div>
+                          <div className="desc-con"></div>
                         </div>
-                        <p>
-                          <strong>Education:</strong>
-                          {friend.education ? friend.education : ""}
-                        </p>
+                        <div className="connection-status-con">
+                          <img
+                            className="chat-icon"
+                            onClick={openChatWindow}
+                            src={chatIcon}
+                            alt=""
+                          />
+                        </div>
+                        <div className="connection-status-con">
+                          <img
+                            className="chat-icon"
+                            onClick={() => openProfileDetails(friend._id)}
+                            src={viewProfileIcon}
+                            alt=""
+                          />
+                        </div>
                       </div>
-                      <div className="desc-con"></div>
-                    </div>
-                    <div className="connection-status-con">
-                      <img
-                        className="chat-icon"
-                        onClick={openChatWindow}
-                        src={chatIcon}
-                        alt=""
-                      />
-                    </div>
-                    <div className="connection-status-con">
-                      <img
-                        className="chat-icon"
-                        onClick={() => openProfileDetails(friend._id)}
-                        src={viewProfileIcon}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                ))
-              : "Make some friends"}
-          </div>
+                    ))
+                  : "Make some friends"}
+              </div>
+            </div>
+          )}
         </div>
+        <div className="pagination">
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            activeClassName="active"
+          />
+        </div>
+        <ToastContainer />
       </div>
-      <div className="pagination">
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel="next >"
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={pageCount}
-          previousLabel="< previous"
-          renderOnZeroPageCount={null}
-          containerClassName="pagination"
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          previousLinkClassName="page-link"
-          nextClassName="page-item"
-          nextLinkClassName="page-link"
-          breakClassName="page-item"
-          breakLinkClassName="page-link"
-          activeClassName="active"
-        />
-      </div>
-      <ToastContainer />
     </>
   );
 };
